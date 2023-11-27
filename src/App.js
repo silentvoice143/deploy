8621231fc622d4435ef26f58096026de94d5a5d5
Satyam Kumar <silentvoice4143@gmail.com>
2023-11-27 14:57:36 +0530

    demo deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  function Home() {
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    );
+  }
+
+  function About() {
+    return (
+      <div>
+        <h1>About</h1>
+      </div>
+    );
+  }
+
+  function Social() {
+    return (
+      <div>
+        <h1>Social</h1>
+      </div>
+    );
+  }
+
+  function Contact() {
+    return (
+      <div>
+        <h1>Contact</h1>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/social">Social</Link>
+        </header>
+      </div>
+
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/about" Component={About} />
+        <Route exact path="/contact" Component={Contact} />
+        <Route exact path="/Social" Component={Social} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
